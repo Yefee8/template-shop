975d51431ce143450b3897604cef725592ef43df
Yavuz Efe <yavuzefefindikli8@gmail.com>
2022-08-02 17:24:28 +0300

    footer margin changed, title and description bug fixed 🚀

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,15 @@
 <template>
   <div>
     <Head>
-      <title>template shop! - buy some new templates!</title>
-      <meta name="description" content="buy some templates from template shop!">
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.2/css/all.min.css" integrity="sha512-1sCRPdkRXhBV2PBLUdRb4tMg1w2YPf37qatUFeS7zlBy7jJI8Lf4VHwWfZZfpXtYSLy85pkm9GaYVYMfw5BC1A==" crossorigin="anonymous" referrerpolicy="no-referrer">
     </Head>
     <Header />
 
     <Main />
 
-    <Packs class="mt-4 md:mt-8"/>
+    <Packs class="mt-4 md:mt-8" />
 
-    <Footer class="mt-4 md:mt-8"/>
+    <Footer class="mt-8 md:mt-16" />
   </div>
 </template>
 
@@ -19,7 +17,20 @@
 import Vue from 'vue'
 
 export default Vue.extend({
-  name: 'IndexPage'
+  name: 'IndexPage',
+  head () {
+    return {
+      title: 'template shop - buy some new templates!',
+      meta: [
+        // hid is used as unique identifier. Do not use `vmid` for it as it will not work
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'buy some templates from template shop!'
+        }
+      ]
+    }
+  }
 })
 </script>
 
